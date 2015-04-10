2742c56c84ec2a199c49c01a3ae01e61273c7b44
Alex Lemm <alex23lemm@gmail.com>
2015-04-10 17:42:04 +0200

    Add backend logic

---
+++ server.R
@@ -0,0 +1,34 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(jsonlite)
+library(RCurl)
+library(magrittr)
+library(yaml)
+
+
+config <- yaml.load_file("config.yml")
+
+shinyServer(function(input, output) {
+  
+  output$scoring <- renderText({
+    
+    scoring <- data.frame(male = input$gender, age = input$age, currentSmoker = input$smoker,
+               totChol = input$totChol, sysBP = input$sysBP, 
+               glucose = input$glucose) %>% 
+      toJSON %>%
+       paste0(config$base_url, "?record=", .) %>% gsub('\"', "%22", .) %>%
+      getURL(., ssl.verifypeer = FALSE, 
+              userpwd = paste0(config$usr, ":", config$pwd)) %>% fromJSON %$% outputs %>% as.numeric 
+  })
+})
+
+
+#what <- getURL(gsub('\"', "%22", paste0(base_url, "?record=", query )), 
+#               ssl.verifypeer = FALSE, userpwd = "adapa:i-9749ef9a") %>% fromJSON %$%
+
