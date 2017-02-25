18f3a11482f4992fdf94447ba477631365469ad2
Alex Lemm <alex23lemm@gmail.com>
2017-02-25 23:22:04 +0100

    Replace RCurl with httr

--- server.R
+++ server.R
@@ -1,13 +1,13 @@
 
-# This is the server logic for a Shiny web application.
+# This is the server logic for a Shiny web application. 	
 # You can find out more about building applications with Shiny here:
 #
-# http://shiny.rstudio.com
+# http://shiny.rstudio.com 
 #
 
 library(shiny)
 library(jsonlite)
-library(RCurl)
+library(httr)
 library(magrittr)
 library(yaml)
 
@@ -18,21 +18,23 @@ shinyServer(function(input, output) {
   
   output$scoring <- renderText({
     
-    scoring <- "-"
     
-    if(input$submitButton > 0){
-      scoring <- isolate(data.frame(male = input$gender, age = input$age, currentSmoker = input$smoker,
-                                      totChol = input$totChol, sysBP = input$sysBP, 
-                                      glucose = input$glucose) %>% 
-                             toJSON %>%
-                             paste0(config$base_url, "?record=", .) %>% 
-                           gsub('\"', "%22", .) %>%
-                           getURL(., ssl.verifypeer = FALSE, 
-                                    userpwd = paste0(config$usr, ":", config$pwd)) %>%
-                           fromJSON %$% outputs %>% as.numeric %>% 
-                           round(digits = 4) *100) %>% paste("%") 
-    }
-    scoring 
+    # Take a dependency on input$goButton
+    if (input$submitButton == 0)
+      return("-")
+    
+    record <- isolate(data.frame(male = input$gender, age = input$age, 
+                          currentSmoker = input$smoker,
+                          totChol = input$totChol, sysBP = input$sysBP, 
+                          glucose = input$glucose) %>% toJSON)
+    response <- GET(config$base_url,
+                    authenticate(config$usr, config$pwd),
+                    query = list(record = record), content_type_json()) %>% 
+      content(as = "text") %>% fromJSON %>% .[["outputs"]] %$%
+      as.numeric(Predicted_TenYearCHD) %>% round(digits = 4) * 100
+    response <- paste(response, "%")
+                        
+    response
   })
 })
 
